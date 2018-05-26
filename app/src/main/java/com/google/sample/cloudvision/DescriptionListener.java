66b3381a361566a9c569394d15154c908d1e7284
yuhyeok <yuhyeok.jeon@gmail.com>
2018-05-26 17:00:41 -0400

    Added text to speech, swipe listener, and description listener classes

--- app/src/main/java/com/google/sample/cloudvision/DescriptionListener.java
+++ app/src/main/java/com/google/sample/cloudvision/DescriptionListener.java
@@ -3,24 +3,75 @@ package com.google.sample.cloudvision;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.google.api.client.googleapis.auth.clientlogin.ClientLogin;
+
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Calendar;
+import java.util.Iterator;
 
 public class DescriptionListener extends AsyncTask<String, String, String> {
 
     int startTime = 0; // seconds
     String description = "defaultDescription";
+    URL url;
+    HttpURLConnection connection;
+    JSONObject jsonObject;
 
     @Override
     protected String doInBackground(String... params) {
         startTime = Calendar.getInstance().getTime().getSeconds();
 
-        while(description.equals("defaultDescription") && Calendar.getInstance().getTime().getSeconds() - startTime < 30)
-        {
-            if (Calendar.getInstance().getTime().getSeconds() - startTime > 1)
-            {
-                description = "something new";
+        boolean areNewEntriesAvailable = false;
+
+        InputStream in;
+
+        try {
+            url = new URL("https://enghack-firebase.firebaseio.com/.json");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(false);
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            connection.connect();
+            Thread.sleep(10000);
+        } catch (Exception e) {
+            Log.e("DB", "Connection Exception: " + e.getMessage());
+        }
+
+        try {
+            in = new BufferedInputStream(connection.getInputStream());
+            BufferedReader rd = new BufferedReader(new InputStreamReader(in));
+            String content = "", line;
+            while ((line = rd.readLine()) != null) {
+                content += line + "\n";
             }
-            // check for description
+            jsonObject = new JSONObject(content);
+            JSONObject messages = jsonObject.getJSONObject("messages");
+            Iterator<?> keys = messages.keys();
+            String lastKey = "";
+            int tempNumEntries = 0;
+            while(keys.hasNext())
+            {
+                tempNumEntries++;
+                lastKey = (String)keys.next();
+            }
+            JSONObject lastEntry = messages.getJSONObject(lastKey);
+            description = lastEntry.getString("Description");
+            MainActivity.descriptionEntries = tempNumEntries;
+        }
+        catch(Exception e)
+        {
+            Log.e("DB", "Exception: " + e.getMessage());
         }
 
         publishProgress(description);
@@ -40,6 +91,7 @@ public class DescriptionListener extends AsyncTask<String, String, String> {
 
     @Override
     protected void onProgressUpdate(String... text) {
-        Log.d("Loop", text[0]);
+        Log.d("Description: " , text[0]);
+        //MainActivity.tts.speak(text[0]);
     }
 }
